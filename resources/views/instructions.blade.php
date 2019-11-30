6e76f7c4498b3aad6c73e6032d2c702405dea387
Defry Socop <venturadef@gmail.com>
2019-11-29 23:24:58 -0600

    tirdh commit

--- resources/views/instructions.blade.php
+++ resources/views/instructions.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="panel panel-primary">
-    <div class="panel-heading">Sistema de gestión de incidencias</div>
+    <div class="panel-heading">Sistema de gestión de incidencias 1</div>
     <div class="panel-body text-center">
         <p>Página de instrucciones. Pendiente por editar.</p>
     </div>
